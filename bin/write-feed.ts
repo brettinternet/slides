ea9d883bd2200120f58c68cf6beaf8793563b721
brett <brettinternet@gmail.com>
2020-12-12 13:31:14 -0700

    build(write-feed): restructure output

--- bin/write-feed.ts
+++ bin/write-feed.ts
@@ -1,16 +1,11 @@
 import { outputJsonSync } from 'fs-extra'
 import { resolve } from 'path'
-import getMeta = require('../config/templates')
+import compile = require('../config/templates')
 import paths = require('../config/paths')
 
-const meta = getMeta()
+const meta = compile()
 
-const feed = meta.reduce(
-  (acc, slide) => ({
-    ...acc,
-    [slide.slug]: slide.frontmatter,
-  }),
-  {}
+outputJsonSync(
+  resolve(paths.build, 'feed.json'),
+  meta.map(({ frontmatter }) => frontmatter)
 )
-
-outputJsonSync(resolve(paths.build, 'feed.json'), feed)
