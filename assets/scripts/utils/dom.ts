d6372d5fc795c52986e1d8740e8d24d1bc6fc076
brett <20871604+brettinternet@users.noreply.github.com>
2025-08-08 15:41:15 -0600

    consolidate style helpers

--- assets/scripts/utils/dom.ts
+++ assets/scripts/utils/dom.ts
@@ -45,3 +45,14 @@ export const inIframe = () => {
     return true
   }
 }
+
+/**
+ * Match tailwind config
+ */
+export const breakpoints = {
+  xs: 481,
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+}
